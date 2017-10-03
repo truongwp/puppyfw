8f1aa9eec34db355a97fd4b7aa031796880e68ef
Truong Giang <truongwp@gmail.com>
2017-10-03 23:45:45 +0700

    Add alias of get option api

--- loader.php
+++ loader.php
@@ -66,3 +66,24 @@ function puppyfw_load_plugin_textdomain() {
 	load_plugin_textdomain( 'puppyfw', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 }
 add_action( 'plugins_loaded', 'puppyfw_load_plugin_textdomain' );
+
+
+/**
+ * Gets option api.
+ * This method automatically get default value if value is not set.
+ *
+ * @since 0.2.0
+ *
+ * @param  string $page_slug Option page slug.
+ * @param  string $option_id Option ID.
+ * @return mixed
+ */
+function presserva_get_option( $page_slug, $option_id ) {
+	$page = puppyfw()->get_page( $page_slug );
+
+	if ( ! $page ) {
+		return null;
+	}
+
+	return $page->get_option( $option_id );
+}
